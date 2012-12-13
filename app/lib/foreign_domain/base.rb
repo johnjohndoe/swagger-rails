0f894d5401557081162e8a37e132da54762884c9
marsz <marsz330@gmail.com>
2012-12-14 03:04:32 +0800

    fix long subdomain error

--- app/lib/foreign_domain/base.rb
+++ app/lib/foreign_domain/base.rb
@@ -21,7 +21,8 @@ module ForeignDomain::Base
     private
 
     def get_doc_by_request
-      @doc = Doc.find_by_fqdn(request.host) || Doc.find_by_fqdn("#{request.host}:#{request.port}") || Doc.find_by_subdomain(request.subdomain)
+      subdomain = request.subdomain.to_s.split(".").first
+      @doc = Doc.find_by_fqdn(request.host) || Doc.find_by_fqdn("#{request.host}:#{request.port}") || Doc.find_by_subdomain(subdomain)
       response.headers["Access-Control-Allow-Origin"] = "*"
       render :text => "", :status => 500 unless @doc
     end
